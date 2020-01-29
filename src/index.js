43036e021c7c8ac4b182807ff5776455f517387e
Grilo <bernardo.aires@mutual.club>
2020-01-29 16:50:31 -0300

    semi-homework

--- src/index.js
+++ src/index.js
@@ -1,22 +1,56 @@
-const express = require('express');
-const app = express();
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const express = require('express')
+const app = express()
 
-app.post('/accounts', function (req, res) {
-    res.send('Hello World!');
-});
+const url = 'mongodb://localhost:27017'
 
-app.post('/matches', function (req, res) {
-    res.send('Hello World!');
-});
+const dbName = 'PingPong'
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+const client = new MongoClient(url, { useUnifiedTopology: true })
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+const getDb = async () => {
+    if (!client.isConnected()) {
+        client.connect(function(err) {
+            assert.equal(null, err)
+            console.log("Connected successfully to server")
+        })
+    } 
+    const db = await client.db(dbName)
+    
+    return db
+}
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.post('/', async (req, res) => {
+    // const { username, password, name, email, age, sex, profilePicture, ranking } = req
+    const { name, age } = req
+    const db = await getDb()
+    // const player = { username, password, name, email, age, sex, profilePicture, ranking }
+    const player = { name, age }
+    const newPlayer = await db.collection('Player').insertOne(player)
+    res.send(newPlayer)
+    const { data, player1Id, player2Id, winnerId, result, points } = req
+    const match = { data, player1Id, player2Id, winnerId, result, points }
+    const newMatch = await db.collection('Match').insertOne(match)
+    res.send(newMatch)
+})
+
+app.get('/players', async (req, res) => {
+    const db = await getDb()
+    const players = await db.collection('Player').find({})
+    res.send(players)
+})
+
+app.get('/players/:playerId', async (req, res) => {
+    const db = await getDb()
+    const player = await db.collection('Player').findOne({})
+    res.send(player)
+})
+
+app.get('/matches/:matchesId', async (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+})
